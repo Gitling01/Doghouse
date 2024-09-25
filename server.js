f1cd4fd37d9c3a190740e5eb7be262ab17eb2131
Gitling01 <Faylor52@gmail.com>
2024-09-24 23:35:01 -0400

    Create db connection and Modify /data route to respond with listing properties

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
+const connection = require('./db.js');
 const express = require('express');
-const app = express()
-const cors = require('cors')
-const path = require('path')
-app.use(express.json())
-app.use(cors())
-app.use('/static', express.static(path.join(__dirname, 'public')))
+const app = express();
+const cors = require('cors');
+const path = require('path');
+app.use(express.json());
+app.use(cors());
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-const port = 3000
+const port = 3000;
 
-app.get('/data', (req, res)=> {
-    res.send("Hi");
+//DB RELATED ROUTES
+let db;
+connection().then(connection => {
+    db = connection;
+}).catch(err => {
+    console.error('Failed to connect to the database.',err);
+});
+
+app.get('/data', async (req,res) => {
+    if(!db){
+      return res.status(500).json({ error: 'Database connection not made.' });  
+    }
+    const query = "SELECT street_address, price, bedroom_quantity, bathroom_quantity, photo_url, size FROM DoghouseDB.listing";
+    try{
+        const [results] = await db.execute(query); 
+        res.json(results); 
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    }
 })
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
 
-TODO: Update GET /data to get values for the street_address, price, photo_url,
-bedroom_quantity, bathroom_quantity, and size
