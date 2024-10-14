50063e6c751e630d9c8d6c5c3f47a0386c528727
Gitling01 <Faylor52@gmail.com>
2024-10-14 18:35:29 -0400

    Add registration routes to add users to actual database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const bcrypt = require('bcrypt');
 app.use(express.json());
 app.use(cors());
 app.use('/static', express.static(path.join(__dirname, 'public')));
@@ -29,6 +30,42 @@ app.get('/data', async (req,res) => {
         res.status(500).json({ error: err.message });
     }
 })
+
+//LOGIN RELATED ROUTES
+app.get('/users', async (req,res) => { //TODO: REVIEW
+    if(!db){
+        return res.status(500).json({ error: 'Database connection not made.' });  
+      }
+      const query = "SELECT username,user_password,email FROM DoghouseDB.user";
+      try{
+          const [results] = await db.execute(query); 
+          res.json(results); 
+      } catch(err) {
+          res.status(500).json({ error: err.message });
+      }
+});
+
+app.post('/users', async(req,res) => { //TODO: REVIEW
+    if(!db){
+        return res.status(500).json({ error: 'Database connection not made.' });  
+      }
+    try{
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password,salt);
+        const username = req.body.username; 
+        const email = req.body.email;
+        const query = "INSERT INTO DoghouseDB.user (username, user_password, email) VALUES (?,?,?)";
+        await db.execute(query,[username,hashedPassword,email]);     
+        res.status(201).send();     
+    } catch(error){
+        console.error("Error adding user", error);
+        res.status(500).send();
+    }
+});
+
+app.post('/users/login',(res,req) => {
+    //TODO: add route to let users login
+});
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
