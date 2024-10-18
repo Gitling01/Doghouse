8a759c1a86c39994b37c2d764d73e5e9291cfc07
Gitling01 <Faylor52@gmail.com>
2024-10-17 22:31:55 -0400

    Fix listing creation bug

--- public/index.js
+++ public/index.js
@@ -1,19 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
     getListingData();
-    
-    //login-register forms
-    const loginBody = document.getElementById('login-body');
-    const registerBody = document.getElementById('register-body');
+
+//login-register forms
     const registerLink = document.getElementById('register-link');
     const loginLink = document.getElementById('login-link');
-    registerLink.addEventListener('click', function() {
-        toggleForm(loginBody,registerBody)});
-    loginLink.addEventListener('click', function() {
-        toggleForm(loginBody,registerBody);});
+    const loginBody = document.getElementById('login-body');
+    const registerBody = document.getElementById('register-body');
+    if(registerLink){
+         registerLink.addEventListener('click', function() {
+        toggleForm(loginBody, registerBody)});
+    }
+    if(loginLink){
+        loginLink.addEventListener('click', function() {
+        toggleForm(loginBody, registerBody);});
+    }
+    
+     //listing-form
+    const listingForm = document.getElementById('listing-form');
+    listingForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createListing();
+        listingForm.reset();
+    });
+   
 })
 
+//TODO: can modify it to get city and zipcode too  
 async function getListingData(){
-    await fetch('http://localhost:3000/data')
+    await fetch('http://localhost:3000/listings')
     .then(response => response.json())
     .then(data => {
         data.forEach((item, index) => { 
@@ -37,6 +51,40 @@ async function getListingData(){
     .catch(error => console.error(error)); 
 }
 
+async function createListing(){
+    const streetAddress = document.getElementById("street-address").value;
+    const city = document.getElementById("city").value;
+    const zipcode = document.getElementById("zipcode").value;
+    const price = parseInt(document.getElementById("price").value);
+    const numberOfBedrooms = parseInt(document.getElementById("number-of-bedrooms").value);
+    const numberOfBathrooms = parseInt(document.getElementById("number-of-bathrooms").value);
+    const photoUrl = document.getElementById("photo-url").value;
+    const size = parseInt(document.getElementById("size").value);
+    console.log("In createListing: " + streetAddress + " " + city + " " + zipcode + " " + price + " " +
+         numberOfBedrooms + " " + numberOfBathrooms + " " + size + " " + photoUrl);
+    await fetch("http://localhost:3000/listings", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"street_address": streetAddress, "city": city, 
+            "zipcode": zipcode, "price": price, "bedroom_quantity": numberOfBedrooms,
+            "bathroom_quantity": numberOfBathrooms, "photo_url": photoUrl, "size": size
+        })
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Bad response " + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Successfully posted", data);
+    })
+    .catch(error => console.error("Error creating listing in createListing function", error))
+}
+
+//TODO: headers, stringify, more logging
 async function registerUser(){
     const username = document.getElementById("username");
     const password = document.getElementById("password");
