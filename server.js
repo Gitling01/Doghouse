ba9c4cdc9cc72c9a82e1238c432590ad8a2169d9
Gitling01 <Faylor52@gmail.com>
2024-10-17 22:26:40 -0400

    Add minor logging and response handling for '/listings' POST route debugging

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.json());
 app.use(cors());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-//DB RELATED ROUTES
+//CONNECT TO DB
 let db;
 connection().then(connection => {
     db = connection;
@@ -18,11 +18,12 @@ connection().then(connection => {
     console.error('Failed to connect to the database.',err);
 });
 
-app.get('/data', async (req,res) => {
+//GET LISTINGS
+app.get('/listings', async (req,res) => {
     if(!db){
       return res.status(500).json({ error: 'Database connection not made.' });  
     }
-    const query = "SELECT street_address, price, bedroom_quantity, bathroom_quantity, photo_url, size FROM DoghouseDB.listing";
+    const query = "SELECT street_address, city, zipcode, price, bedroom_quantity, bathroom_quantity, photo_url, size FROM DoghouseDB.listing";
     try{
         const [results] = await db.execute(query); 
         res.json(results); 
@@ -31,18 +32,44 @@ app.get('/data', async (req,res) => {
     }
 })
 
+//POST LISTING
+app.post('/listings', async (req,res) => {
+    if(!db){
+        return res.status(500).json({ error: 'Database connection not made'});
+    }
+    console.log("Request body: ", req.body);
+    try{
+        const streetAddress = req.body.street_address;
+        const city = req.body.city;
+        const zipcode = req.body.zipcode;
+        const price = req.body.price;
+        const bedroomQuantity = req.body.bedroom_quantity;
+        const bathroomQuantity = req.body.bathroom_quantity;
+        const photoUrl = req.body.photo_url;
+        const size = req.body.size;
+        console.log("In post route: " + streetAddress + " " + city + " " + zipcode + " " + price + " " +
+            + bedroomQuantity + " " + bathroomQuantity + " " + size + " " + photoUrl);
+        const query = "INSERT INTO DoghouseDB.listing (street_address, city, zipcode, price, bedroom_quantity, bathroom_quantity, photo_url, size) VALUES (?,?,?,?,?,?,?,?)";
+        await db.execute(query,[streetAddress, city, zipcode, price, bedroomQuantity, bathroomQuantity, photoUrl, size]);
+        res.status(201).json({ message: "Listing created successfully!"});
+    } catch(err){
+        console.error("Error creating listing in post route", err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
 //LOGIN RELATED ROUTES
 app.get('/users', async (req,res) => { //TODO: REVIEW
     if(!db){
         return res.status(500).json({ error: 'Database connection not made.' });  
-      }
+    }
       const query = "SELECT username,user_password,email FROM DoghouseDB.user";
-      try{
-          const [results] = await db.execute(query); 
-          res.json(results); 
-      } catch(err) {
-          res.status(500).json({ error: err.message });
-      }
+    try{
+       const [results] = await db.execute(query); 
+       res.json(results); 
+    } catch(err) {
+         res.status(500).json({ error: err.message });
+    }
 });
 
 app.post('/users', async(req,res) => { //TODO: REVIEW
